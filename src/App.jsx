6fc7f2fd8df0b8caefd5876068b5853fa289c756
axellancieri <axel.lavignasse@gmail.com>
2024-11-28 15:29:30 -0300

    made changes so the GameBoard component now is not totally in control of the state regardging the X and the Os on the board and pushed it to the App component so we can now get that information down to the new Log component aswell as the Gameboard component that needs it to function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import { useState } from 'react';
 import Player from './components/Player';
 import GameBoard from './components/Gameboard';
+import Log from './components/Log';
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
 const [activePlayer, setActivePlayer] = useState('X');
 
-function handleSelectSquare() {
+function handleSelectSquare(rowIndex, colIndex) {
   setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+  setGameTurns(prevTurns => {
+    let currentPlayer ='X';
+
+    if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
+      currentPlayer = 'O';
+    }
+
+    const updatedTurns = [
+      { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+      ...prevTurns,
+    ];
+
+    return updatedTurns;
+  });
 }
 
   return (
@@ -17,10 +33,13 @@ function handleSelectSquare() {
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
         </ol>
 
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+        <GameBoard 
+          onSelectSquare={handleSelectSquare}
+          turns={gameTurns}
+        />
       </div>
 
-      LOG
+      <Log />
     </main>
   )
 }
